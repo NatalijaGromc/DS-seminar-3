2d15f04feaa3344bc78f777a42f5cc77f5ac652f
natasha <gromc-natalija@rambler.ru>
2022-10-10 15:55:49 +0300

    дз1

---
+++ DS1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+int N,M,num,temp;
+string userEnter;
+
+Console.Write ("Введите число N: ");
+userEnter = Console.ReadLine()!;
+N = int.Parse(userEnter);
+temp=N;
+M=0;
+while (temp>0)
+{
+    num=temp%10;
+    M=M*10+num;
+    temp=temp/10;
+}
+// Console.WriteLine(M);
+if (N==M)
+{
+Console.Write($"палиндром");
+}
+else
+{
+Console.Write($"нет");
+}
